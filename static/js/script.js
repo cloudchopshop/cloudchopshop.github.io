8e1eb666d1940577ab5db5ff7dbd5257c3fbb1a6
cloudchopshop <ed.davis.j@gmail.com>
2020-04-15 12:07:41 -0400

    tag cloud fix

--- static/js/script.js
+++ static/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     console.log("?");
 
     backToTop();
-    search();
+    // search();
 });
 
 /**
--- static/js/tagCloud.js
+++ static/js/tagCloud.js
@@ -1,4 +1,3 @@
-
 (function($){
     $.fn.tagCloud = function(){
 
